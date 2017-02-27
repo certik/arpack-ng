93dc03bc6b105208400bb121be91a8dd009a0353
Ondřej Čertík <ondrej.certik@gmail.com>
2017-02-26 21:25:48 -0700

    Simplify

--- EXAMPLES/SIMPLE/dssimp.f
+++ EXAMPLES/SIMPLE/dssimp.f
@@ -172,45 +172,15 @@ c
       msaup2 = 0
       mseigt = 0
       mseupd = 0
-c     
-c     %-------------------------------------------------%
-c     | The following sets dimensions for this problem. |
-c     %-------------------------------------------------%
-c
+
       nx = 10
       n = nx
-c
-c     %-----------------------------------------------%
-c     |                                               | 
-c     | Specifications for ARPACK usage are set       | 
-c     | below:                                        |
-c     |                                               |
-c     |    1) NEV = 4  asks for 4 eigenvalues to be   |  
-c     |       computed.                               | 
-c     |                                               |
-c     |    2) NCV = 20 sets the length of the Arnoldi |
-c     |       factorization                           |
-c     |                                               |
-c     |    3) This is a standard problem              |
-c     |         (indicated by bmat  = 'I')            |
-c     |                                               |
-c     |    4) Ask for the NEV eigenvalues of          |
-c     |       largest magnitude                       |
-c     |         (indicated by which = 'LM')           |
-c     |       See documentation in DSAUPD for the     |
-c     |       other options SM, LA, SA, LI, SI.       | 
-c     |                                               |
-c     | Note: NEV and NCV must satisfy the following  |
-c     | conditions:                                   |
-c     |              NEV <= MAXNEV                    |
-c     |          NEV + 1 <= NCV <= MAXNCV             |
-c     %-----------------------------------------------%
-c
+
       nev   = 9
       ncv   = 10
       bmat  = 'I'
       which = 'SM'
-c
+
       if ( n .gt. maxn ) then
          print *, ' ERROR with _SSIMP: N is greater than MAXN '
          go to 9000
@@ -221,212 +191,57 @@ c
          print *, ' ERROR with _SSIMP: NCV is greater than MAXNCV '
          go to 9000
       end if
-c
-c     %-----------------------------------------------------%
-c     |                                                     |
-c     | Specification of stopping rules and initial         |
-c     | conditions before calling DSAUPD                    |
-c     |                                                     |
-c     | TOL  determines the stopping criterion.             |
-c     |                                                     |
-c     |      Expect                                         |
-c     |           abs(lambdaC - lambdaT) < TOL*abs(lambdaC) |
-c     |               computed   true                       |
-c     |                                                     |
-c     |      If TOL .le. 0,  then TOL <- macheps            |
-c     |           (machine precision) is used.              |
-c     |                                                     |
-c     | IDO  is the REVERSE COMMUNICATION parameter         |
-c     |      used to specify actions to be taken on return  |
-c     |      from DSAUPD. (See usage below.)                |
-c     |                                                     |
-c     |      It MUST initially be set to 0 before the first |
-c     |      call to DSAUPD.                                | 
-c     |                                                     |
-c     | INFO on entry specifies starting vector information |
-c     |      and on return indicates error codes            |
-c     |                                                     |
-c     |      Initially, setting INFO=0 indicates that a     | 
-c     |      random starting vector is requested to         |
-c     |      start the ARNOLDI iteration.  Setting INFO to  |
-c     |      a nonzero value on the initial call is used    |
-c     |      if you want to specify your own starting       |
-c     |      vector (This vector must be placed in RESID.)  | 
-c     |                                                     |
-c     | The work array WORKL is used in DSAUPD as           | 
-c     | workspace.  Its dimension LWORKL is set as          |
-c     | illustrated below.                                  |
-c     |                                                     |
-c     %-----------------------------------------------------%
-c
+
       lworkl = ncv*(ncv+8)
       tol = zero 
       info = 0
       ido = 0
-c
-c     %---------------------------------------------------%
-c     | Specification of Algorithm Mode:                  |
-c     |                                                   |
-c     | This program uses the exact shift strategy        |
-c     | (indicated by setting PARAM(1) = 1).              |
-c     | IPARAM(3) specifies the maximum number of Arnoldi |
-c     | iterations allowed.  Mode 1 of DSAUPD is used     |
-c     | (IPARAM(7) = 1). All these options can be changed |
-c     | by the user. For details see the documentation in |
-c     | DSAUPD.                                           |
-c     %---------------------------------------------------%
-c
+
       ishfts = 1
       maxitr = 300 
       mode1 = 1
-c
+
       iparam(1) = ishfts
-c                
       iparam(3) = maxitr
-c                  
       iparam(7) = mode1
-c
-c     %------------------------------------------------%
-c     | M A I N   L O O P (Reverse communication loop) |
-c     %------------------------------------------------%
-c
+
  10   continue
-c
-c        %---------------------------------------------%
-c        | Repeatedly call the routine DSAUPD and take | 
-c        | actions indicated by parameter IDO until    |
-c        | either convergence is indicated or maxitr   |
-c        | has been exceeded.                          |
-c        %---------------------------------------------%
-c
          call dsaupd ( ido, bmat, n, which, nev, tol, resid, 
      &                 ncv, v, ldv, iparam, ipntr, workd, workl,
      &                 lworkl, info )
-c
+
          if (ido .eq. -1 .or. ido .eq. 1) then
-c
-c           %--------------------------------------%
-c           | Perform matrix vector multiplication |
-c           |              y <--- OP*x             |
-c           | The user should supply his/her own   |
-c           | matrix vector multiplication routine |
-c           | here that takes workd(ipntr(1)) as   |
-c           | the input, and return the result to  |
-c           | workd(ipntr(2)).                     |
-c           %--------------------------------------%
-c
             call av (nx, workd(ipntr(1)), workd(ipntr(2)))
-c
-c           %-----------------------------------------%
-c           | L O O P   B A C K to call DSAUPD again. |
-c           %-----------------------------------------%
-c
             go to 10
-c
-         end if 
-c
-c     %----------------------------------------%
-c     | Either we have convergence or there is |
-c     | an error.                              |
-c     %----------------------------------------%
-c
+         end if
       if ( info .lt. 0 ) then
-c
-c        %--------------------------%
-c        | Error message. Check the |
-c        | documentation in DSAUPD. |
-c        %--------------------------%
-c
          print *, ' '
          print *, ' Error with _saupd, info = ', info
          print *, ' Check documentation in _saupd '
          print *, ' '
-c
-      else 
-c
-c        %-------------------------------------------%
-c        | No fatal errors occurred.                 |
-c        | Post-Process using DSEUPD.                |
-c        |                                           |
-c        | Computed eigenvalues may be extracted.    |  
-c        |                                           |
-c        | Eigenvectors may be also computed now if  |
-c        | desired.  (indicated by rvec = .true.)    | 
-c        |                                           |
-c        | The routine DSEUPD now called to do this  |
-c        | post processing (Other modes may require  |
-c        | more complicated post processing than     |
-c        | mode1.)                                   |
-c        |                                           |
-c        %-------------------------------------------%
-c           
+      else
           rvec = .true.
-c
           call dseupd ( rvec, 'All', select, d, v, ldv, sigma, 
      &         bmat, n, which, nev, tol, resid, ncv, v, ldv, 
      &         iparam, ipntr, workd, workl, lworkl, ierr )
-c
-c         %----------------------------------------------%
-c         | Eigenvalues are returned in the first column |
-c         | of the two dimensional array D and the       |
-c         | corresponding eigenvectors are returned in   |
-c         | the first NCONV (=IPARAM(5)) columns of the  |
-c         | two dimensional array V if requested.        |
-c         | Otherwise, an orthogonal basis for the       |
-c         | invariant subspace corresponding to the      |
-c         | eigenvalues in D is returned in V.           |
-c         %----------------------------------------------%
-c
           if ( ierr .ne. 0) then
-c
-c            %------------------------------------%
-c            | Error condition:                   |
-c            | Check the documentation of DSEUPD. |
-c            %------------------------------------%
-c
              print *, ' '
              print *, ' Error with _seupd, info = ', ierr
              print *, ' Check the documentation of _seupd. '
              print *, ' '
-c
           else
-c
              nconv =  iparam(5)
              do 20 j=1, nconv
-c
-c               %---------------------------%
-c               | Compute the residual norm |
-c               |                           |
-c               |   ||  A*x - lambda*x ||   |
-c               |                           |
-c               | for the NCONV accurately  |
-c               | computed eigenvalues and  |
-c               | eigenvectors.  (iparam(5) |
-c               | indicates how many are    |
-c               | accurate to the requested |
-c               | tolerance)                |
-c               %---------------------------%
-c
                 call av(nx, v(1,j), ax)
                 call daxpy(n, -d(j,1), v(1,j), 1, ax, 1)
                 d(j,2) = dnrm2(n, ax, 1)
                 d(j,2) = d(j,2) / abs(d(j,1))
                 d(j,1) = d(j,1) - 10
-c
  20          continue
-c
-c            %-----------------------------%
-c            | Display computed residuals. |
-c            %-----------------------------%
-c
              call dmout(6, nconv, 2, d, maxncv, -6,
      &            'Ritz values and relative residuals')
           end if
-c
-c         %-------------------------------------------%
-c         | Print additional convergence information. |
-c         %-------------------------------------------%
-c
+
           if ( info .eq. 1) then
              print *, ' '
              print *, ' Maximum number of iterations reached.'
@@ -436,7 +251,7 @@ c
              print *, ' No shifts could be applied during implicit',
      &                ' Arnoldi update, try increasing NCV.'
              print *, ' '
-          end if      
+          end if
 c
           print *, ' '
           print *, ' _SSIMP '
